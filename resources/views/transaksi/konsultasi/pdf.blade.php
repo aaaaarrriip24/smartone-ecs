e3bd5912b9789d6553661343fd18a90b0175fd77
aaaaarrriip24 <164979994+aaaaarrriip24@users.noreply.github.com>
2024-08-01 16:16:56 +0700

    Update pdf.blade.php

--- resources/views/transaksi/konsultasi/pdf.blade.php
+++ resources/views/transaksi/konsultasi/pdf.blade.php
@@ -53,7 +53,7 @@ pa
                     <td>{{ date('d-m-Y', strtotime($d->tanggal_konsultasi)) }}/<br>{{ $d->cara_konsultasi }}</td>
                     <td>{{ $d->nama_perusahaan }}, {{ $d->nama_tipe }}<br>{{ $d->nama_petugas }}</td>
                     <td>{{ $d->nama_topik }}</td>
-                    <td width="35%">{{ str_replace("&nbsp;", "", strip_tags($d->isi_konsultasi)) }}</td>
+                    <td width="30%">{{ str_replace("&nbsp;", "", strip_tags($d->isi_konsultasi)) }}</td>
                     <td width="250px">
                         @if(!empty($d->foto_pertemuan))
                         <img class="rounded border" src="{{ public_path('foto_pertemuan/'. $d->foto_pertemuan) }}"
