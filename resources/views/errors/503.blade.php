c9e429f2f7eff283f106c3245fe6deac490de558
aaaaarrriip24 <164979994+aaaaarrriip24@users.noreply.github.com>
2024-08-21 15:44:42 +0700

    error page

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>SmartOne Information</title>
+</head>
+
+<body style="padding: 20px;">
+    <div class="row">
+    </div>
+    <div class="col-sm-12 text-center">
+        <img src="https://smart.osakimedical.co.id/assets/img/err.jpg" style="width: 70%; height:100%;" />
+    </div>
+    <div class="col-sm-12 text-center">
+        <h3 style="font-size: 35px;">Website Under Constraction.</h3>
+    </div>
+
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
